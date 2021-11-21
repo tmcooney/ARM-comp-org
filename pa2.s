6817a0f05628dcef5a855629d492daed1fb2d3d5
Todd Cooney <63822499+tmcooney@users.noreply.github.com>
2021-11-20 20:48:30 -0500

    pseudo control structure in place

--- pa2.s
+++ pa2.s
@@ -40,7 +40,54 @@ main:
     # store y in x20
     ldrsw x20, [sp]
 
+
+    bl power
+    
+
     b exit
+# power function
+power:
+    
+    #if x is not zero
+    cbnz x0, elseIf
+
+    # if y == 0 return 1
+    cbz x1, yIsZero
+
+    # if y is less than zero
+    subs xzr, x1, xzr
+    b.lt yIsNegative
+    # base case: if x==0 return zero
+    cbz x0, xIsZero
+    ret
+
+#else if y < 0
+elseIf:
+    subs xzr, x1, xzr
+    b.ge elseIfIf
+# else if y is not 0
+elseIfIf:
+    cbnz x1, exponent_recurse
+    
+    
+# if y < 0
+yIsNegative:
+    add x2, xzr, xzr
+    add x2, x2, #1
+    ret
+#if y==0
+yIsZero:
+    mov x2, x1
+    ret
+#if x==0
+xIsZero:
+    mov x2, x0
+    ret
+
+
+# recursion function
+exponent_recurse:
+    #do stuff and things here
 
 # branch to this label on program completion
 exit:
